8bdc296a2f6e0e5fd4bc3e5d3d0c124b29bcb674
Henrique Vieira <87788394+henriquecvieira@users.noreply.github.com>
2022-03-28 11:45:36 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Sucessfull connection")
+    }).catch((errror) => {
+        console.log("error")
+    })
+    
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.listen(3333, () => {
+    console.log("Backend started in http://localhost:3333")
+})
