4452fd585ec7509933a8e4a443cd203864ca10fc
Henrique Vieira <87788394+henriquecvieira@users.noreply.github.com>
2022-03-28 14:07:52 -0300

    criação do model Category

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const connection = require("./database/database")
 const categoriesController = require("./categories/CategoriesController")
 const articlesController = require("./articles/ArticlesController")
 
+
+
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
