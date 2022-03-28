0641c82130e3c2f06217eea3402f812fa7a95305
Henrique Vieira <87788394+henriquecvieira@users.noreply.github.com>
2022-03-28 13:59:18 -0300

    arquitetura e organização da aplicação node js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const connection = require("./database/database")
+const categoriesController = require("./categories/CategoriesController")
+const articlesController = require("./articles/ArticlesController")
 
 app.set('view engine', 'ejs')
 
@@ -20,6 +22,9 @@ connection
     })
     
 
+
+app.use("/", categoriesController)
+app.use("/", articlesController)
 
 app.get("/", (req, res) => {
     res.render("index");
