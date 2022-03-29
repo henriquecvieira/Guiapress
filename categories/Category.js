d044924728b12127e11058cf0475a912934d1913
Henrique Vieira <87788394+henriquecvieira@users.noreply.github.com>
2022-03-29 10:39:09 -0300

    deletando categoria

--- categories/Category.js
+++ categories/Category.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize")
 const connection = require("../database/database")
 
-const Category = connection.define('categories', {
+const Category = connection.define("categories", {
     title:{
         type: Sequelize.STRING,
         allowNull: false
