04a5f6f3b3b869cc2c6304918eb640c6ec5f36ee
Henrique Vieira <87788394+henriquecvieira@users.noreply.github.com>
2022-03-30 11:38:09 -0300

    criando página de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,7 +5,7 @@ const Article = require("./Article")
 const slugify = require("slugify")
 
 router.get("/admin/articles", (req, res) => {
-    res.send("ROTA DE artigos")
+    res.render("admin/articles/index")
 })
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
